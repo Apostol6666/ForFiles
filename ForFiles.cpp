4535502e6fe8c73e2ee470359a47dc8d9cbdf8d9
Apostol6666 <44125687+Apostol6666@users.noreply.github.com>
2019-12-23 04:53:16 +0800

    Create ForFiles.cpp

---
+++ ForFiles.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include <iostream>
+#include "head.h"
+
+using namespace std;
+
+int main()
+{
+	Data s;
+//	s.AllNames();
+	s.CompleteFiles();
+    std::cout << "Hello World!\n";
+
+	return 0;
+}
